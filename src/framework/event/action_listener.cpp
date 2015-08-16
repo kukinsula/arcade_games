5d9eda6e5ac6db6c63974a16ee67a8b2b3bec6d7
kukinsula <l.texier10@gmail.com>
2015-08-16 14:58:09 +0200

    remove(ActionListener): remove useless ActionListener and UserAction

--- src/framework/event/action_listener.cpp
+++
@@ -1,5 +0,0 @@
-#include "action_listener.hpp"
-
-void ActionListener::on_mouse_over_widget (EventHandler *, Widget *) {}
-
-void ActionListener::on_mouse_click_on_widget (EventHandler *, Widget *) {}
